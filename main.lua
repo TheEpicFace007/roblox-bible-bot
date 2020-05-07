e51f058937169f35695611f49d6aa9c53371b226
Vini Dalvino <vinidalvino@cock.li>
2020-05-07 12:47:22 -0400

    Replaced `Your program` to bible bot

--- main.lua
+++ main.lua
@@ -421,15 +421,16 @@ coroutine.resume(coroutine.create(function()
     end
 end))
 --[[
- This program is free software: you can redistribute it and/or modify
+ Bible bot is free software: you can redistribute it and/or modify
     it under the terms of the GNU AFFERO General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    Bible bot is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.]]
+    along with Bible bot.  If not, see <https://www.gnu.org/licenses/>.
+--]]
